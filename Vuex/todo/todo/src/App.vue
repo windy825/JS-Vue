6736e9b1b5100b7693c8b7a097027f483f02c2e0
windy82581 <windy82581@gmail.com>
2022-05-16 00:42:53 +0900

    h

---
+++ Vuex/todo/todo/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>App</h1>
+    <h2>전체 : {{ todos }} | 완료 : {{ completedTodosCount }} | 미완료 : {{ uncompletedTodosCount }}</h2>
+    <todo-list></todo-list>
+    <todo-form></todo-form>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import TodoForm from '@/components/TodoForm'
+import { mapGetters } from 'vuex'
+
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    TodoForm,
+  },
+  computed: {
+    // completedddd: function () {
+    //   return this.$store.getters.completedTodosCount
+    // },
+    // uncompletedddd: function () {
+    //   return this.$store.getters.uncompletedTodosCount
+    // },
+    // todos: function () {
+    //   return this.$store.getters.todos
+    // },
+    ...mapGetters([
+      'completedTodosCount',
+      'uncompletedTodosCount',
+      'todos',
+    ])
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
